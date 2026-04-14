fdd00c740a83ce61e65df86c27a63e3de45d88db
Stephen Best <bestie@gmail.com>
2026-04-14 20:13:11 +0200

    Fix CLI tests after color config change

--- tests/cli_opts_test.c
+++ tests/cli_opts_test.c
@@ -82,20 +82,20 @@ Test(cli_opts, parse_color_from_command_line_args) {
 
     parse_command_line_options(argc, argv, &config);
 
-    cr_assert_eq(config.background_color.r, 255, "Red component should be 255");
-    cr_assert_eq(config.background_color.g, 0, "Green component should be 0");
-    cr_assert_eq(config.background_color.b, 255, "Blue component should be 255");
+    cr_assert_eq(config.background_color->r, 255, "Red component should be 255");
+    cr_assert_eq(config.background_color->g, 0, "Green component should be 0");
+    cr_assert_eq(config.background_color->b, 255, "Blue component should be 255");
 
-    cr_assert_eq(config.text_color.r, 0x12, "Red component should be 0x12");
-    cr_assert_eq(config.text_color.g, 0x34, "Green component should be 0x34");
-    cr_assert_eq(config.text_color.b, 0x56, "Blue component should be 0x56");
+    cr_assert_eq(config.text_color->r, 0x12, "Red component should be 0x12");
+    cr_assert_eq(config.text_color->g, 0x34, "Green component should be 0x34");
+    cr_assert_eq(config.text_color->b, 0x56, "Blue component should be 0x56");
 
-    cr_assert_eq(config.text_selected_color.r, 0x67, "Red component should be 0x67");
-    cr_assert_eq(config.text_selected_color.g, 0x89, "Green component should be 0x89");
-    cr_assert_eq(config.text_selected_color.b, 0xAB, "Blue component should be 0xAB");
+    cr_assert_eq(config.text_selected_color->r, 0x67, "Red component should be 0x67");
+    cr_assert_eq(config.text_selected_color->g, 0x89, "Green component should be 0x89");
+    cr_assert_eq(config.text_selected_color->b, 0xAB, "Blue component should be 0xAB");
 
-    cr_assert_eq(config.text_selected_background_color.r, 0xAB, "Red component should be 0xAB");
-    cr_assert_eq(config.text_selected_background_color.g, 0xCD, "Green component should be 0xCD");
-    cr_assert_eq(config.text_selected_background_color.b, 0xEF, "Blue component should be 0xEF");
+    cr_assert_eq(config.text_selected_background_color->r, 0xAB, "Red component should be 0xAB");
+    cr_assert_eq(config.text_selected_background_color->g, 0xCD, "Green component should be 0xCD");
+    cr_assert_eq(config.text_selected_background_color->b, 0xEF, "Blue component should be 0xEF");
 
 }
